4cbc2f4133191f13f679911c892630f73f4ef868
amandineRoger <aroger@excilys.com>
2016-04-21 12:10:46 +0200

    Format end comment _ CLI

--- src/control/CLI.java
+++ src/control/CLI.java
@@ -6,23 +6,20 @@ import view.TerminalView;
 
 public class CLI {
 	public static Scanner scan = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		scan.useDelimiter("\\n");
 		int menuCommand = -1;
-		
-		while (menuCommand != 0){
+
+		// Execution loop
+		while (menuCommand != 0) {
 			TerminalView.displayMenu();
 			menuCommand = scan.nextInt();
-			
-			if (!Master.menuManager(menuCommand)){
+
+			if (!Master.menuManager(menuCommand)) {
 				TerminalView.displayTypingError();
 			}
 		}
 		scan.close();
 	}
-	
-	
-	
-
 }
