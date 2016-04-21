0f2759c30465fed47f8dae29264fa8e18b54e37c
amandineRoger <aroger@excilys.com>
2016-04-21 12:09:51 +0200

    Format and comments _ SingleConnect

--- src/bdd/SingleConnect.java
+++ src/bdd/SingleConnect.java
@@ -5,66 +5,65 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SingleConnect {
-	//Constants
+	// Constants
 	private static final String ADRESS = "jdbc:mysql://127.0.0.1:3306/computer-database-db?zeroDateTimeBehavior=convertToNull";
 	private static final String USERNAME = "admincdb";
 	private static final String PASSWORD = "qwerty1234";
-	
+
 	private static final String LOG_TAG = "SingleConnect says : ";
-	
-	//Attributes
+
+	// Attributes
 	private Connection connect;
 	private StringBuffer sb;
-	
-	//Getters
+
+	// Getters
 	/**
 	 * Open db connection, you must to close it later !
+	 * 
 	 * @return java.sql.Connection
 	 */
-	public Connection getConnection(){
+	public Connection getConnection() {
 		try {
 			this.connect = DriverManager.getConnection(ADRESS, USERNAME, PASSWORD);
 		} catch (SQLException e) {
 			sb.append(" SQLException ");
 			sb.append(e.getMessage());
-			System.out.println( sb );
+			System.out.println(sb);
 			resetStringBuffer();
 		}
 		return this.connect;
 	}
-	
-	
-	//Singleton
+
+	// Singleton
 	private static SingleConnect instance = null;
-	
-	private SingleConnect(){
+
+	private SingleConnect() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			sb = new StringBuffer(LOG_TAG);
 			sb.append(" ClassNotFoundException ! ");
 			sb.append(e.getMessage());
-			System.out.println( sb );
+			System.out.println(sb);
 			resetStringBuffer();
 		}
 	}
-	
+
 	/**
 	 * GetInstance, singleton pattern
+	 * 
 	 * @return the unique instance of SingleConnection
 	 */
-	public static SingleConnect getInstance(){
+	public static SingleConnect getInstance() {
 		if (instance == null) {
 			instance = new SingleConnect();
 		}
 		return instance;
 	}
-	
-	
-	//Methods
-	private void resetStringBuffer(){
+
+	// Methods
+	private void resetStringBuffer() {
 		sb.delete(LOG_TAG.length(), sb.length());
 	}
-	
-	
+
 }
