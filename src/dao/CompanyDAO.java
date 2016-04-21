f059b96200e8199944c60bae861763a8489629d5
amandineRoger <aroger@excilys.com>
2016-04-21 12:23:35 +0200

    Javadoc

--- src/dao/CompanyDAO.java
+++ src/dao/CompanyDAO.java
@@ -10,27 +10,31 @@ import bdd.SingleConnect;
 import entities.Company;
 
 public class CompanyDAO {
-	
+
 	private static final String TABLE = "company";
-	
 	private static final int COL_ID = 1;
 	private static final int COL_NAME = 2;
-	
+
 	private SingleConnect singleConnect;
 	private Connection connect;
-	
-	public CompanyDAO(){
+
+	public CompanyDAO() {
 		this.singleConnect = SingleConnect.getInstance();
 	}
-	
-	public ArrayList<Company> getCompanyList(){
+
+	/**
+	 * Get all companies from database
+	 * 
+	 * @return ArrayList<Company> all companies
+	 */
+	public ArrayList<Company> getCompanyList() {
 		ArrayList<Company> list = new ArrayList<>();
-		
-		//Query
-		String query = "SELECT * from "+TABLE;
+
+		// Query
+		String query = "SELECT * from " + TABLE;
 		ResultSet results = null;
 		connect = singleConnect.getConnection();
-		
+
 		try {
 			PreparedStatement ps = connect.prepareStatement(query);
 			results = ps.executeQuery();
@@ -39,31 +43,34 @@ public class CompanyDAO {
 				tmp = new Company();
 				tmp.setId(results.getLong(COL_ID));
 				tmp.setName(results.getString(COL_NAME));
-				
 				list.add(tmp);
-				
 			}
-			
 			connect.close();
-			
 		} catch (SQLException e) {
 			System.out.println("Company DAO says : getCompanyList _ " + e.getMessage());
 		}
-		
+
 		return list;
 	}
-	
-	public Company getCompanyById(long id){
+
+	/**
+	 * Find a company by its id
+	 * 
+	 * @param id
+	 *            id of the wanted company
+	 * @return wanted company
+	 */
+	public Company getCompanyById(long id) {
 		Company company = null;
-		if (id != 0){
+		if (id != 0) {
 			StringBuffer query = new StringBuffer("SELECT name FROM ");
 			query.append(TABLE);
 			query.append(" WHERE id = ");
 			query.append(id);
-			
+
 			connect = singleConnect.getConnection();
-			
 			PreparedStatement ps;
+
 			try {
 				ps = connect.prepareStatement(query.toString());
 				ResultSet rs = ps.executeQuery();
@@ -78,5 +85,5 @@ public class CompanyDAO {
 		}
 		return company;
 	}
-	
+
 }
