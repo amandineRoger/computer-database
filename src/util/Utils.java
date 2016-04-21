636bb4f4d8e2f76d983924c1a006e632ac1a90ba
amandineRoger <aroger@excilys.com>
2016-04-21 13:52:29 +0200

    Javadoc and Format _ Utils

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -6,25 +6,33 @@ import java.time.LocalDate;
 public class Utils {
 
 	/*
-	 * SQL types found on : 
-	 * http://alvinalexander.com/java/edu/pj/jdbc/recipes/ResultSet-ColumnType.shtml
+	 * SQL types found on :
+	 * http://alvinalexander.com/java/edu/pj/jdbc/recipes/ResultSet-ColumnType.
+	 * shtml
 	 */
 	public static final int SQLTYPE_NULL = 0;
 	public static final int SQLTYPE_BIGINT = -5;
 	public static final int SQLTYPE_VARCHAR = 12;
 	public static final int SQLTYPE_TIMESTAMP = 93;
-	
+
 	public static final int CURRENT_YEAR = LocalDate.now().getYear();
-	
-	public static LocalDate timeStampToLocalDate(Timestamp ts){
-		 LocalDate date;
-		 
-		 if(ts == null) {
-			 date = null;
-		 } else {
-			 date = ts.toLocalDateTime().toLocalDate();
-		 }
-		 
-		 return date;
-	 }
+
+	/**
+	 * Convert sql.Timestamp into time.LocalDate
+	 * 
+	 * @param ts
+	 *            sql.Timestamp to convert
+	 * @return LocalDate
+	 */
+	public static LocalDate timeStampToLocalDate(Timestamp ts) {
+		LocalDate date;
+
+		if (ts == null) {
+			date = null;
+		} else {
+			date = ts.toLocalDateTime().toLocalDate();
+		}
+
+		return date;
+	}
 }
