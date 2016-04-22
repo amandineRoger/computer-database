d3896d75000df54a927e52cd58fd299c8005a832
amandineRoger <aroger@excilys.com>
2016-04-22 10:59:49 +0200

    Add Util file for SQL Queries

---
+++ src/util/UtilQuerySQL.java
@@ -0,0 +1,15 @@
+package util;
+
+public interface UtilQuerySQL {
+	public static final String COMPUTER_TABLE = "computer";
+	public static final String COMPANY_TABLE = "company";
+	
+	
+	//Rename constants?
+	public static final int COL_ID = 1;
+	public static final int COL_NAME = 2;
+	public static final int COL_INTRODUCED = 3;
+	public static final int COL_DISCONTINUED = 4;
+	public static final int COL_COMPANY_ID = 5;
+	public static final int COL_COMPANY_NAME = 6;
+}
